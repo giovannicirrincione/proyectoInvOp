bb6e4b62daef0f73a1578701dbae3c1706c13c6f
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-05-30 14:34:35 -0300

    todo funcionando ok

--- back/src/main/java/proyectoInvOp/back/Entity/Articulo.java
+++ back/src/main/java/proyectoInvOp/back/Entity/Articulo.java
@@ -39,9 +39,8 @@ public class Articulo extends Base {
     @JoinColumn(name = "familiaArticuloId")
     private FamiliaArticulo familiaArticulo;
 
-    @JsonIgnore
     @ManyToOne(cascade = CascadeType.REFRESH,fetch = FetchType.EAGER)
-    @JoinColumn(name = "provedorDeterminadoId")
+    @JoinColumn(name = "proveedorPredeterminadoId")
     private Proveedor proveedorPredeterminado;
 
     @JsonIgnore
