64626f69ca4c7e08a961aee153be0a19495e398b
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-05-30 12:57:42 -0300

    controladores recuperados

---
+++ back/src/main/java/proyectoInvOp/back/Controller/EstadoOrdenCompraController.java
@@ -0,0 +1,16 @@
+package proyectoInvOp.back.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import proyectoInvOp.back.Entity.Articulo;
+import proyectoInvOp.back.Entity.EstadoOrdenCompra;
+import proyectoInvOp.back.Services.ArticuloServiceImpl;
+import proyectoInvOp.back.Services.EstadoOrdenCompraServiceImpl;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "estadoOC")
+public class EstadoOrdenCompraController extends BaseControllerImpl<EstadoOrdenCompra, EstadoOrdenCompraServiceImpl>{
+}
