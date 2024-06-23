a13cf87e2457fc29315a6bc4d88344b6775fb4ba
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-06-23 19:38:44 -0300

    controller, service y repos de parametros

--- back/src/main/java/proyectoInvOp/back/Repositories/ParametrosEspecificosRepository.java
+++ back/src/main/java/proyectoInvOp/back/Repositories/ParametrosEspecificosRepository.java
@@ -1,8 +1,8 @@
 package proyectoInvOp.back.Repositories;
 
 import org.springframework.stereotype.Repository;
-import proyectoInvOp.back.Entity.Base;
 import proyectoInvOp.back.Entity.ParametroEspecifico;
+
 @Repository
-public interface ParametrosEspecificosRepository extends BaseRepository<ParametroEspecifico,Long> {
+public interface ParametrosEspecificosRepository extends BaseRepository<ParametroEspecifico,Long>{
 }
---
+++ back/src/main/java/proyectoInvOp/back/Services/ParametroEspecificoService.java
@@ -0,0 +1,7 @@
+package proyectoInvOp.back.Services;
+
+import proyectoInvOp.back.Entity.Articulo;
+import proyectoInvOp.back.Entity.ParametroEspecifico;
+
+public interface ParametroEspecificoService extends BaseService<ParametroEspecifico,Long>{
+}
